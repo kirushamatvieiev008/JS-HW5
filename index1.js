914a46311457de3f0b89d37e179534773cc2e329
Кірюша <mkiril087@gmail.com>
2024-09-12 10:30:47 +0300

    Update index1.js

--- index1.js
+++ index1.js
@@ -88,39 +88,121 @@
 
 // task 3
 
-// const age = Number(prompt(`enter your age`));
-// let description;
+// const age = prompt(`enter age`);
+// let category;
 
-// if (age >= 0 && age < 13) {
-//     description = `child`;
-// } else if (age >= 13 && age < 18) {
-//     description = `teenager`;
-// } else if (age >= 18 && age < 65) {
-//     description = `adult`;
-// } else {
-//     description = `old person`
+// switch (true) {
+//     case (age >= 0 && age <= 12):
+//         category = "Дитина";
+//         break;
+//     case (age >= 13 && age <= 17):
+//         category = "Підліток";
+//         break;
+//     case (age >= 18 && age <= 64):
+//         category = "Дорослий";
+//         break;
+//     case (age >= 65):
+//         category = "Пенсіонер";
+//         break;
+//     default:
+//         category = "Невірний вік";
 // }
 
-// confirm(description);
+// console.log(category);
 
-const age = prompt(`enter age`);
-let category;
+// task 4
+
+
+// const numOne = parseInt(prompt(`enter first number`));
+// let x = prompt(`enter * or / or + or -`);
+// const numTwo  = parseInt(prompt(`enter second number`));
+// let y;
+// // y = numOne * numTwo;
+
+// switch (x) {
+//     case x = `*`:
+//         y = numOne * numTwo;
+//         break;
+//         case x = `/`:
+//             y = numOne / numTwo;
+//             break;
+//             case x = `+`:
+//                 y = numOne + numTwo;
+//                 break;
+//                 case x = `-`:
+//                     y = numOne - numTwo;
+//         break;
+//         default:
+            
+// }
+        
+// console.log(y);
+
+
+// task 5
+
+// const season = Number(prompt(`enter num from 1 to 12`));
+// let thisSeason;
+
+// switch (season) {
+//     case 1:
+//         thisSeason = `January`;
+//         break;
+//     case 2:
+//         thisSeason = `February`;
+//         break;
+//     case 3:
+//         thisSeason = `March`;
+//         break;
+//     case 4:
+//         thisSeason = `April`;
+//         break;
+//     case 5:
+//         thisSeason = `May`;
+//         break;
+//     case 6:
+//         thisSeason = `june`;
+//         break;
+//     case 7:
+//         thisSeason = `July`;
+//         break;
+//     case 8:
+//         thisSeason = `August`;
+//         break;
+//     case 9:
+//         thisSeason = `September`;
+//         break;
+//     case 10:
+//         thisSeason = `October`;
+//         break;
+//     case 11:
+//         thisSeason = `November`;
+//         break;
+//     case 12:
+//         thisSeason = `December`;
+//         break;
+//     default:
+// }
+
+// confirm(thisSeason);
+
+// task 6
+
+const personal = prompt(`enter how good we were, відмінно or добре or задовільно`);
+let teaMoney = 'відмінно';
 
 switch (true) {
-    case (age >= 0 && age <= 12):
-        category = "Дитина";
+    case personal = `відмінно`:
+        teaMoney = `you have to pay 20% of all price to teaMoney`;
         break;
-    case (age >= 13 && age <= 17):
-        category = "Підліток";
-        break;
-    case (age >= 18 && age <= 64):
-        category = "Дорослий";
-        break;
-    case (age >= 65):
-        category = "Пенсіонер";
-        break;
-    default:
-        category = "Невірний вік";
-}
+        case personal = `добре`:
+            teaMoney = `you have to pay 15% of all price to teaMoney`;
+            break;
+            case personal = `задовільно`:
+                teaMoney = `you have to pay 10% of all price to teaMoney`;
+                break;
+                default:
+                    
+                }
+                console.log(teaMoney);
 
-console.log(category);
